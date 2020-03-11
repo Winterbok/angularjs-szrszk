ffb0b845b78ed8fb21b4b33ff22bfd2c4f8f093b
GrondAndy <apjgrond@gmail.com>
2020-03-11 23:55:30 +0100

    2 files modified

--- home/home.controller.js
+++ home/home.controller.js
@@ -2,11 +2,17 @@ class HomeCtrl {
   constructor($scope) {
     'ngInject';
 
+    this.tableItems = [];
+
     this.name = "AngularJS";
   }
 
   reset($scope){
     this.name = "AngularJS";
+  }
+
+  addItem($scope){
+    this.tableItems.push(this.name);
   }
 
 }
